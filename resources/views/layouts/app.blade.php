8f8ba50562ce6d4eedc5b14e9042adf7d2e663fc
ilhammmaulana <ilhammmaulana688@gmail.com>
2023-07-26 05:55:26 +0700

    fix - Layouting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,15 +7,11 @@
     <title>
         Voyage App
     </title>
-    <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-    <!-- Nucleo Icons -->
     <link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
     <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
-    <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
-    <!-- CSS Files -->
     <link id="pagestyle" href="assets/css/argon-dashboard.css" rel="stylesheet" />
 </head>
 
--- resources/views/pages/destinations.blade.php
+++ resources/views/pages/destinations.blade.php
@@ -16,7 +16,6 @@
                             Create Destination
                         </button>
 
-                        <!-- Modal -->
                         <div class="modal fade" id="createDestination" tabindex="-1" role="dialog"
                             aria-labelledby="createModelDestination" aria-hidden="true">
                             <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
